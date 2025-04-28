28983a93cbf844c684300a7224d9c03e51de3b9a
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-04-28 10:54:56 +0700

    class

---
+++ class2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Mobil
+{
+public:
+    string nama_mobil;
+    string tipe_mobil;
+
+private:
+    int harga_mobil;
+
+public:
+    void input_data(){
+        cout << "masukkan nama mobil: ";
+        cin >> nama_mobil;
+        cout << "masukkan tipe mobil: ";
+        cin >> tipe_mobil;
+        cout << "masukkan harga mobil: ";
+        cin >> harga_mobil;
+    }
+    void output_data(){
+        cout << "haraga mobil = " << harga_mobil << endl;
+
+    }
+
+};
+int main()
+{
+    Mobil nazar;
+    nazar.input_data();
+    cout << "nama mobil = " << nazar.nama_mobil << endl;
+    cout << "tipe mobil = " << nazar.tipe_mobil << endl;
+    nazar.output_data();
+    
+}
